a5645a6a13bf50fd4e56980e2d30d0c9434e23d5
Alonso <rafael.a.m95@hotmail.com>
2019-10-12 18:23:46 +0000

    changes file struct to split styles in diferent file

--- front-end/bus-schedule/src/Pages/Login/index.js
+++ front-end/bus-schedule/src/Pages/Login/index.js
@@ -11,9 +11,7 @@ import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-
-import ImgSignIn from '../../assets/SignIn.jpg'
+import {useStyles} from './styles';
 
 function Copyright() {
     return (
@@ -27,35 +25,6 @@ function Copyright() {
       </Typography>
     );
   }
-  
-  const useStyles = makeStyles(theme => ({
-    root: {
-      height: '100vh',
-    },
-    image: {
-      backgroundImage: `url(${ImgSignIn})`,
-      backgroundRepeat: 'no-repeat',
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-    },
-    paper: {
-      margin: theme.spacing(8, 4),
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-    },
-    avatar: {
-      margin: theme.spacing(1),
-      backgroundColor: theme.palette.secondary.main,
-    },
-    form: {
-      width: '100%', // Fix IE 11 issue.
-      marginTop: theme.spacing(1),
-    },
-    submit: {
-      margin: theme.spacing(3, 0, 2),
-    },
-  }));
   
 
 export default function Login() {
---
+++ front-end/bus-schedule/src/Pages/Login/styles.js
@@ -0,0 +1,35 @@
+
+
+import ImgSignIn from '../../assets/SignIn.jpg'
+
+import { makeStyles } from '@material-ui/core/styles';
+const useStyles = makeStyles(theme => ({
+    root: {
+      height: '100vh',
+    },
+    image: {
+      backgroundImage: `url(${ImgSignIn})`,
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+    },
+    paper: {
+      margin: theme.spacing(8, 4),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
+
+  export { useStyles }
